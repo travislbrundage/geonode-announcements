eea68b30e602c2cc0603716e9bf257209f10848d
Patrick Altman <paltman@gmail.com>
2013-01-24 17:18:20 -0600

    Comply with what auth backends expect
    It needs to have an authenticate method, for our
    purposes this is just a noop.

--- announcements/auth_backends.py
+++ announcements/auth_backends.py
@@ -1,16 +1,11 @@
-from django.db.models import Q
-
-from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth.models import User
-
-from account.models import EmailAddress
-
-
-class AnnouncementPermissionsBackend(ModelBackend):
+class AnnouncementPermissionsBackend(object):
     supports_object_permissions = True
     supports_anonymous_user = True
+    
+    def authenticate(self, **kwargs):
+        # always return a None user
+        return None
     
     def has_perm(self, user, perm, obj=None):
         if perm == "announcements.can_manage":
             return user.is_authenticated() and user.is_staff
-        return super(AnnouncementPermissionsBackend, self).has_perm(user, perm)

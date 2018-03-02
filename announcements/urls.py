3e524868ae41a864c37a2f155ddd9fc42698f079
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-03-02 11:34:09 +0100

    Compatibility with Django 1.9+

--- announcements/urls.py
+++ announcements/urls.py
@@ -1,17 +1,13 @@
-try:
-    from django.conf.urls import url, patterns
-except ImportError:
-    from django.conf.urls.defaults import url, patterns
+from django.conf.urls import url
 
 from announcements.views import detail, dismiss
 from announcements.views import CreateAnnouncementView, UpdateAnnouncementView
 from announcements.views import DeleteAnnouncementView, AnnouncementListView
 
-urlpatterns = patterns("",
+urlpatterns = [  # "",
     url(r"^$", AnnouncementListView.as_view(), name="announcements_list"),
     url(r"^announcement/create/$", CreateAnnouncementView.as_view(), name="announcements_create"),
     url(r"^announcement/(?P<pk>\d+)/$", detail, name="announcements_detail"),
     url(r"^announcement/(?P<pk>\d+)/hide/$", dismiss, name="announcements_dismiss"),
     url(r"^announcement/(?P<pk>\d+)/update/$", UpdateAnnouncementView.as_view(), name="announcements_update"),
     url(r"^announcement/(?P<pk>\d+)/delete/$", DeleteAnnouncementView.as_view(), name="announcements_delete"),
-)
